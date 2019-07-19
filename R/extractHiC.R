9d0a28ea4928d91a466764fff8e96571b91ec57d
Nicole Kramer <nekramer@live.unc.edu>
2019-07-19 15:06:58 -0400

    update gridplotHic and extractHiC

--- R/extractHiC.R
+++ R/extractHiC.R
@@ -1,31 +1,73 @@
 #' @export
+#sourceCpp('~/Desktop/straw-master/R/straw-R.cpp')
 
-extractHiC <- function(hic, region, resolution, zrange=NULL, norm="NONE"){
+extractHiC <- function(hic, chrom, chromstart, chromend, resolution, plottype = "square", zrange=NULL, norm="NONE", altchrom = NULL, altchromstart = NULL, altchromend = NULL){
   ## Parse region input for straw
-  regionSplit <- unlist(strsplit(unlist(strsplit(region, ":")), "-"))
-  regionCleaned <- gsub(pattern = "chr|chrom|CHR|CHROM", replacement = "", x = regionSplit)
-  regionStraw <- paste(regionCleaned, collapse = ":")
   
+  
+  #parameter = region
+  # regionSplit <- unlist(strsplit(unlist(strsplit(region, ":")), "-"))
+  # print(regionSplit)
+  # regionCleaned <- gsub(pattern = "chr|chrom|CHR|CHROM", replacement = "", x = regionSplit)
+  # print(regionCleaned)
+  # regionStraw <- paste(regionCleaned, collapse = ":")
+  
+  regionChrom <- gsub(pattern = "chr", replacement = "", x = chrom)
+  regionStraw <- paste(regionChrom, chromstart, chromend, sep = ":")
+
   ## Extract upper triangular using straw; reverse for lower triangular; combine unique
-  upper <- straw_R(sprintf("%s %s %s %s BP %i", norm, hic, regionStraw, regionStraw, resolution))
-  lower <- upper[, c(2,1,3)]
-  colnames(lower) <- c("x", "y", "counts")
-  combined <- unique(rbind(upper, lower))
-  
-  ## Complete missing values; convert missing values to 0 counts
-  combinedComplete <- tidyr::complete(combined, x, y)
-  combinedComplete$counts[is.na(combinedComplete$counts)] <- 0
-  
-  ## Scale lower and upper bounds using zrange
-  if(is.null(zrange)){
-    zrange <- c(0, max(combinedComplete$counts))
-    combinedComplete$counts[combinedComplete$counts <= zrange[1]] <- zrange[1]
-    combinedComplete$counts[combinedComplete$counts >= zrange[2]] <- zrange[2]
+  if(is.null(altchrom)){
+    upper <- straw_R(sprintf("%s %s %s %s BP %i", norm, hic, regionStraw, regionStraw, resolution))
+
   } else {
-    stopifnot(is.vector(zrange), length(zrange) == 2, zrange[2] > zrange[1])
-    combinedComplete$counts[combinedComplete$counts <= zrange[1]] <- zrange[1]
-    combinedComplete$counts[combinedComplete$counts >= zrange[2]] <- zrange[2]
+    
+    regionChrom2 <- gsub(pattern = "chr", replacemtn = "", x = altchrom)
+    regionStraw2 <- paste(regionChrom2, altchromstart, altchromend, sep = ":" )
+    upper <- straw_R(sprintf("%s %s %s %s BP %i", norm, hic, regionStraw, regionStraw2, resolution))
+    
   }
   
-  return(combinedComplete)
+  ## Complete missing values; convert missing values to 0 counts
+  if(plottype == "square"){
+    lower <- upper[, c(2,1,3)]
+    colnames(lower) <- c("x", "y", "counts")
+    combined <- unique(rbind(upper, lower))
+    combinedComplete <- tidyr::complete(combined, x, y)
+    combinedComplete$counts[is.na(combinedComplete$counts)] <- 0
+    ## Scale lower and upper bounds using zrange
+    if(is.null(zrange)){
+      zrange <- c(0, max(combinedComplete$counts))
+      combinedComplete$counts[combinedComplete$counts <= zrange[1]] <- zrange[1]
+      combinedComplete$counts[combinedComplete$counts >= zrange[2]] <- zrange[2]
+    } else {
+      stopifnot(is.vector(zrange), length(zrange) == 2, zrange[2] > zrange[1])
+      combinedComplete$counts[combinedComplete$counts <= zrange[1]] <- zrange[1]
+      combinedComplete$counts[combinedComplete$counts >= zrange[2]] <- zrange[2]
+    }
+    
+    return(as.data.frame(combinedComplete))
+    
+  } else {
+  
+    ## Scale lower and upper bounds using zrange
+    if(is.null(zrange)){
+      zrange <- c(0, max(upper$counts))
+      upper$counts[upper$counts <= zrange[1]] <- zrange[1]
+      upper$counts[upper$counts >= zrange[2]] <- zrange[2]
+    } else {
+      stopifnot(is.vector(zrange), length(zrange) == 2, zrange[2] > zrange[1])
+      upper$counts[upper$counts <= zrange[1]] <- zrange[1]
+      upper$counts[upper$counts >= zrange[2]] <- zrange[2]
+
+    }
+    
+    return(as.data.frame(upper))
+  }
+
+
 }
+
+
+#hicdata <- extractHiC("/Users/phanstiel7/Documents/mega_inter_30.hic", chrom = "chr8", chromstart = chromstart, chromend = chromend, resolution = 5000, zrange=c(0,12), plottype="triangle")
+
+
--- R/gridplotHic.R
+++ R/gridplotHic.R
@@ -1,76 +1,185 @@
 #' @export
 
-gridplotHic <- function(hicdata, chrom, chromstart, chromend, palette, zrange, resolution, height = 3.25, width=3.25, x= 4.25, y= 5.5,...){
+gridplotHic <- function(hic, chrom, chromstart, chromend, palette = 'reds', zrange = NULL, resolution, plottype = "square", half = NULL, raster = TRUE, addlegend = FALSE, legendlocation = NULL, height = 3.25, width=3.25, x= 0.75, y= 0.75,...){
 
-  #assuming input data is of format x, y, counts where x and y are in basepairs
-
-  drawpoly <- function(df, res, chrstart, chrend){
-    col = rgb(df[4],df[5],df[6],maxColorValue=255)
+  ### DEFINE A FUNCTION TO PLOT SQUARES ###
+  drawpoly <- function(df, resolution, chromstart, chromend){
+    
+    ## Define the color
+    col = rgb(df[4], df[5], df[6], maxColorValue=255)
     x = df[1]
     y = df[2]
     
-    #normalized to 0 to 1 based on chromstart and chromend
-    xleft = x-.5*res
-    xleft.normalized = normalize(xleft, chrstart, chrend)
-    xright = x+.5*res
-    xright.normalized = normalize(xright, chrstart, chrend)
-    ytop = y+.5*res
-    ytop.normalized = normalize(ytop, chrstart, chrend)
-    ybottom = y-.5*res
-    ybottom.normalized = normalize(ybottom, chrstart, chrend)
-    #plot all the squares
-    grid.polygon(x=c(xleft.normalized,xleft.normalized,xright.normalized,xright.normalized),y=c(ybottom.normalized,ytop.normalized,ytop.normalized,ybottom.normalized),gp=gpar(col=col,fill=col))
+    ## Get coordinates for the points of the square, normalized to 0 to 1 based on chromstart and chromend
+    xleft = x-.5*resolution
+    xleft.normalized = normalize(xleft, chromstart, chromend)
+    xright = x+.5*resolution
+    xright.normalized = normalize(xright, chromstart, chromend)
+    ytop = y+.5*resolution
+    ytop.normalized = normalize(ytop, chromstart, chromend)
+    ybottom = y-.5*resolution
+    ybottom.normalized = normalize(ybottom, chromstart, chromend)
     
+    ## Plot all the squares
+    grid.polygon(x=c(xleft.normalized,xleft.normalized,xright.normalized,xright.normalized),y=c(ybottom.normalized,ytop.normalized,ytop.normalized,ybottom.normalized),gp=gpar(col=col,fill=col))
+    }
+  
+  ### CHECK WHAT KIND OF DATA WE HAVE AND GET SUBSETTED DATAFRAME ###
+  if (class(hic) == "data.frame"){
+    ## Make sure dataframe is correctly subsetted and has correct header names
+    if(ncol(hic) > 3){
+      stop("Incorrect dataframe format.  Input a dataframe with 3 columns: x, y, counts.")
+    }
+    
+    hicregion <- hic[which(hic[,1] >= chromstart & hic[,1] <= chromend &
+                             hic[,2] >= chromstart & hic[,2] <= chromend),]
+    colnames(hicregion) <- c("x", "y", "counts")
+    
+    ## Scale lower and upper bounds using zrange
+    if(is.null(zrange)){
+      zrange <- c(0, max(hicregion$counts))
+      hicregion$counts[hicregion$counts <= zrange[1]] <- zrange[1]
+      hicregion$counts[hicregion$counts >= zrange[2]] <- zrange[2]
+    } else {
+      stopifnot(is.vector(zrange), length(zrange) == 2, zrange[2] > zrange[1])
+      hicregion$counts[hicregion$counts <= zrange[1]] <- zrange[1]
+      hicregion$counts[hicregion$counts >= zrange[2]] <- zrange[2]
+    }
+    
+  } else if (class(hic) == "character"){
+    ## Make sure the inputted file is a .hic file
+    if(file_ext(hic) == "hic"){
+      hicregion <- extractHiC(hic, chrom = chrom, chromstart = chromstart, chromend = chromend, plottype = plottype, resolution = resolution, zrange = zrange)
+    } else {
+      stop("Incorrect input. Input a .hic file or a dataframe.")
+    } 
+  } else {
+    
+    stop("Incorrect input. Input a .hic file or a dataframe.")
   }
   
-  ###SUBSET DATA###
-  hicregion = hicdata[which(hicdata[,1] >= chromstart & hicdata[,1] <= chromend &
-                                     hicdata[,2] >= chromstart & hicdata[,2] <= chromend),]
- 
-  ###CONVERT NUMBERS TO COLORS###
   
-  #convert data based on zrange
-  min_z = zrange[1]
-  max_z = zrange[2]
-  hicregion[,3][which(hicregion[,3] < min_z)] = min_z
-  hicregion[,3][which(hicregion[,3] > max_z)] = max_z
-  
-  #use colour_values function from "colourvalues" package to convert numbers to colors
-  color_vector <- colour_values(hicregion[,3], palette = palette)
-  
-  #append colors to hicdata and convert to rgb
-  hicregion= cbind(hicregion,t(col2rgb(color_vector)))
-  
-  #DRAW SQUARES###
-  
-  # make symmetric
-  hicregionrev = hicregion[,c(2,1,3,4,5,6)]
-  hicregion = rbind(hicregion,setNames(hicregionrev,names(hicregion))) 
-  hicregion = hicregion[!duplicated(hicregion),]
-  
-  
+  ### VIEWPORTS ###
   
   if(is.null(current.vpPath()) == FALSE){
     upViewport()
   }
-  #make viewport
+  ## Make viewport
   converted_coords = convert_coordinates(height, width, x, y)
   vp <- viewport(height=unit(height,"in"),width=unit(width,"in"),x=unit(converted_coords[1],"in"),y=unit(converted_coords[2],"in"))
   pushViewport(vp)
- 
-  apply(hicregion,1,drawpoly,res=resolution,chrstart = chromstart, chrend=chromend)
+
   
-  #return(list(c(min_z,max_z),palette))
+  ## CONVERT NUMBERS TO COLORS ##
+  ## Use colour_values function from "colourvalues" package to convert numbers to colors
+  color_vector <- colour_values(hicregion[,3], palette = palette)
+  # Sorted color vector for use in legend
+  sorted_color_vector <- colour_values(sort(hicregion[,3]), palette = palette)
+  sorted_colors <- unique(sorted_color_vector)
+  
+  
+  ## RASTERIZED PLOT ##
+  if(raster == TRUE){
+    
+    ## Add color vector to hicregion dataframe
+    hicregion <- cbind(hicregion,color_vector)
+    
+    ## Get the lowest color in the range to fill in matrix
+    lowest_colors <- subset(hicregion, counts == min(counts))
+    lowest_color <- as.character(lowest_colors[1,"color_vector"])
+    
+    ## Remove unnecessary "counts" column
+    hicregion= hicregion[,c(1,2,4)]
+    
+    ## Cast dataframe into a matrix
+    reshapen <- as.matrix(reshape::cast(hicregion,formula=x~y,value="color_vector"))
+    
+    if(plottype == "triangle"){
+      
+      ## Fill in all NA's with the lowest color in the range (essentially a 0)
+      reshapen[is.na(reshapen)] <- lowest_color
+      
+      ## Replace the lower triangular part of the matrix with NA's to not have anything plot there
+      reshapen[lower.tri(reshapen)] <- NA
+      
+      ## Reverse orientation of matrix based on columns for bottom triangle and rows for top triangle
+      if (half == "bottom"){
+        reshapen <- apply(reshapen,2,rev)
+      } else if (half == "top"){
+        reshapen <- apply(reshapen,1,rev)
+      } else {
+        stop("argument \"half\" is required for argument \"plottype = triangle\". ")
+      }
+    } 
+    
+    
+    ## Matrix already complete from extraction, reverse orientation based on columns
+    if(plottype == "square"){
+      reshapen <- apply(reshapen,2,rev)
+    }
+   
+    grid.raster(reshapen)
+    
+  } 
+  
+  
+  ### NON-RASTERIZED PLOT ###
+  if (raster == "FALSE") {
+    
+    ## Append colors to hicdata and convert to rgb
+    hicregion <- cbind(hicregion,t(col2rgb(color_vector)))
+
+    if(plottype == "triangle"){
+      
+      ## Need to get lower part of dataframe to plot bottom triangle
+      if(half == "bottom"){
+        
+        lower <- hicregion[, c(2,1,3,4,5,6)]
+        colnames(lower) <- c("x", "y", "counts", "red", "green", "blue")
+        hicregion <- lower
+      }
+    }
+    
+    
+    ## Plot squares with drawpoly function
+    invisible(apply(hicregion, 1, drawpoly, resolution = resolution, chromstart = chromstart, chromend = chromend))
+
+  }
+  
+  ### LEGEND ###
+  
+  if (addlegend == TRUE){
+    
+
+  
+    min_z <- zrange[1]
+    max_z <- zrange[2]
+
+    if(legendlocation == "right"){
+      legend_width <- 1/32 * width
+      legend_height <- 1/4 * height
+      legend_xcoord <- x + width + 0.25
+      legend_ycoord <- y
+      
+      gridaddLegend(color_vector = sorted_colors, min_label = min_z, max_label = max_z, height= legend_height, width = legend_width, x = legend_xcoord, y = legend_ycoord, location = "right")
+
+    }
+    
+    if(legendlocation == "top"){
+      legend_width <- 1/4 * height
+      legend_height <- 1/32 * width
+      legend_xcoord <- x + (width - legend_width)/2
+      legend_ycoord <- y - 0.25 - legend_height
+      
+      gridaddLegend(color_vector = sorted_colors, min_label = min_z, max_label = max_z, height= legend_height, width = legend_width, x = legend_xcoord, y = legend_ycoord, location = "top")
+    }
+  
+  }
+  
+ 
+  #return sorted_color vector if choosing to add legend later
+  return(sorted_colors) 
 }
 
-# gridplotHic(hicdata = hic.filt.full,
-#          chrom = "chr8",
-#          chromstart = chromstart,
-#          chromend = chromend,
-#          palette  = 'viridis',
-#          resolution = 5000,
-#          zrange=c(0,12),
-#          x= 0.75,
-#          y= 0.75
-# )
+
 
