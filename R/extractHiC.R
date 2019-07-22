9663e5fe0d783354a68c2d805bfce638c3237f74
Nicole Kramer <nekramer@live.unc.edu>
2019-07-22 13:24:57 -0400

    added additional options to extractHiC function

--- R/extractHiC.R
+++ R/extractHiC.R
@@ -1,40 +1,61 @@
+#' extracts HiC data from .hic file using Straw
+#' 
+#' 
+#' @param hic path to .hic file
+#' @param format format of data wanted, which will depend on what kind of plot is ultimately desired; options are "sparse" and "full"
+#' @param chrom if not alternative chromosome, chromosome of desired region
+#' @param chromstart chromosome start position of chrom, in bp
+#' @param chromend chromosome end position of chrom, in bp
+#' @param resolution the width in bp of each pixel
+#' @param zrange the range of interaction scores to plot, where extreme values will be set to the max or min
+#' @param norm hic data normalization; options are "NONE", "VC", "VC_SQRT", and "KR"
+#' @param resscale scale of normalization; options are "BP" and "FRAG"
+#' @param altchrom if looking at region between two different chromosomes, this is the specified alternative chromsome
+#' @param altchromstart if looking at region between two different chromosomes, start position of altchrom
+#' @param altchromend if looking at region between two different chromsomes, end position of altchrom
+
+
 #' @export
 #sourceCpp('~/Desktop/straw-master/R/straw-R.cpp')
 
-extractHiC <- function(hic, chrom, chromstart, chromend, resolution, plottype = "square", zrange=NULL, norm="NONE", altchrom = NULL, altchromstart = NULL, altchromend = NULL){
-  ## Parse region input for straw
+extractHiC <- function(hic, format, chrom, chromstart = NULL, chromend = NULL, resolution, zrange = NULL, norm = "NONE", resscale = "BP", altchrom = NULL, altchromstart = NULL, altchromend = NULL){
   
-  
-  #parameter = region
-  # regionSplit <- unlist(strsplit(unlist(strsplit(region, ":")), "-"))
-  # print(regionSplit)
-  # regionCleaned <- gsub(pattern = "chr|chrom|CHR|CHROM", replacement = "", x = regionSplit)
-  # print(regionCleaned)
-  # regionStraw <- paste(regionCleaned, collapse = ":")
-  
-  regionChrom <- gsub(pattern = "chr", replacement = "", x = chrom)
-  regionStraw <- paste(regionChrom, chromstart, chromend, sep = ":")
 
-  ## Extract upper triangular using straw; reverse for lower triangular; combine unique
-  if(is.null(altchrom)){
-    upper <- straw_R(sprintf("%s %s %s %s BP %i", norm, hic, regionStraw, regionStraw, resolution))
-
+  # Parse chromosome and region in format for Straw
+  if ((is.null(chromstart) & !is.null(chromend)) | (is.null(chromend) & !is.null(chromstart))){
+    stop("Cannot have one \'NULL\' chromstart or chromend.")
+  } else if (is.null(chromstart) & is.null(chromend)){
+    regionStraw <- gsub(pattern = "chr", replacement = "", x = chrom)
   } else {
-    
-    regionChrom2 <- gsub(pattern = "chr", replacemtn = "", x = altchrom)
-    regionStraw2 <- paste(regionChrom2, altchromstart, altchromend, sep = ":" )
-    upper <- straw_R(sprintf("%s %s %s %s BP %i", norm, hic, regionStraw, regionStraw2, resolution))
-    
+    regionChrom <- gsub(pattern = "chr", replacement = "", x = chrom)
+    regionStraw <- paste(regionChrom, chromstart, chromend, sep = ":")
   }
   
-  ## Complete missing values; convert missing values to 0 counts
-  if(plottype == "square"){
-    lower <- upper[, c(2,1,3)]
+  # Extract upper triangular using straw, depending on one chromsome interaction or multiple chromosome interactions 
+  if(is.null(altchrom)){
+    upper <- straw_R(sprintf("%s %s %s %s %s %i", norm, hic, regionStraw, regionStraw, resscale, resolution))
+  } else {
+    if ((is.null(altchromstart) & !is.null(altchromend)) | (is.null(altchromend) & !is.null(altchromstart))){
+      stop("Cannot have one \'NULL\' altchromstart or altchromend.")
+    } else if (is.null(altchromstart) & is.null(altchromend)){
+      regionStraw2 <- gsub(pattern = "chr", replacement = "", x = altchrom)
+    } else {
+      regionChrom2 <- gsub(pattern = "chr", replacement = "", x = altchrom)
+      regionStraw2 <- paste(regionChrom2, altchromstart, altchromend, sep = ":" )
+    }
+    
+    upper <- straw_R(sprintf("%s %s %s %s %s %i", norm, hic, regionStraw, regionStraw2, resscale, resolution))
+  }
+  
+  # Full format: get symmetric data, complete missing values, replace NA's with 0's
+  if(format == "full"){
+    lower <- upper[ ,c(2,1,3)]
     colnames(lower) <- c("x", "y", "counts")
     combined <- unique(rbind(upper, lower))
     combinedComplete <- tidyr::complete(combined, x, y)
     combinedComplete$counts[is.na(combinedComplete$counts)] <- 0
-    ## Scale lower and upper bounds using zrange
+    
+    # Scale lower and upper bounds using zrange
     if(is.null(zrange)){
       zrange <- c(0, max(combinedComplete$counts))
       combinedComplete$counts[combinedComplete$counts <= zrange[1]] <- zrange[1]
@@ -44,11 +65,11 @@ extractHiC <- function(hic, chrom, chromstart, chromend, resolution, plottype = 
       combinedComplete$counts[combinedComplete$counts <= zrange[1]] <- zrange[1]
       combinedComplete$counts[combinedComplete$counts >= zrange[2]] <- zrange[2]
     }
-    
     return(as.data.frame(combinedComplete))
-    
-  } else {
+  } 
   
+  # Sparse format: upper sparse triangular format, scale with new zrange if given
+  if (format == "sparse"){
     ## Scale lower and upper bounds using zrange
     if(is.null(zrange)){
       zrange <- c(0, max(upper$counts))
@@ -58,16 +79,10 @@ extractHiC <- function(hic, chrom, chromstart, chromend, resolution, plottype = 
       stopifnot(is.vector(zrange), length(zrange) == 2, zrange[2] > zrange[1])
       upper$counts[upper$counts <= zrange[1]] <- zrange[1]
       upper$counts[upper$counts >= zrange[2]] <- zrange[2]
-
     }
-    
     return(as.data.frame(upper))
   }
-
-
 }
 
-
-#hicdata <- extractHiC("/Users/phanstiel7/Documents/mega_inter_30.hic", chrom = "chr8", chromstart = chromstart, chromend = chromend, resolution = 5000, zrange=c(0,12), plottype="triangle")
 
 
