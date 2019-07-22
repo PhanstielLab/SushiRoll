a8e4f59cbe09ed6255a8f3e999200821a7f7c0d5
Nicole Kramer <nekramer@live.unc.edu>
2019-07-22 14:40:26 -0400

    added straw compiler

--- R/extractHiC.R
+++ R/extractHiC.R
@@ -19,7 +19,6 @@
 #sourceCpp('~/Desktop/straw-master/R/straw-R.cpp')
 
 extractHiC <- function(hic, format, chrom, chromstart = NULL, chromend = NULL, resolution, zrange = NULL, norm = "NONE", resscale = "BP", altchrom = NULL, altchromstart = NULL, altchromend = NULL){
-  print(getwd())
 
   # Parse chromosome and region in format for Straw
   if ((is.null(chromstart) & !is.null(chromend)) | (is.null(chromend) & !is.null(chromstart))){
@@ -83,6 +82,3 @@ extractHiC <- function(hic, format, chrom, chromstart = NULL, chromend = NULL, r
     return(as.data.frame(upper))
   }
 }
-
-
-
